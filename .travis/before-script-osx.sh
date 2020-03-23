7bf5ee343a79a8120143ae223c8334f4e98b6890
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-22 19:53:56 -0700

    clean up logspam

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-echo "** starting before-script-osx.sh"
 cmake -G Xcode ..
-echo "** done with before-script-osx.sh"
 
--- .travis/script-osx.sh
+++ .travis/script-osx.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "** starting script-osx.sh"
 cmake --build . --config Release
 cmake --install . --config Release
 
