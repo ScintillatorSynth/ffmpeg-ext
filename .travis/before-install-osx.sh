fca7177026ff785ed958c72029496d3c1933acea
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-22 21:25:17 -0700

    fixup tar argument order on deploy script (#2)

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -3,7 +3,7 @@
 export HOMEBREW_NO_ANALYTICS=1
 
 brew update
-brew unlink pyton@2
+brew unlink python@2
 brew install ccache lame libass shtool nasm
 
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
