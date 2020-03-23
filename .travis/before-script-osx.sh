1c3b3949def98587f96e3c7dac5fa3554f48bb0e
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-22 19:34:57 -0700

    adding some temp logspan

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo "** starting before-script-osx.sh"
 cmake -G Xcode ..
+echo "** done with before-script-osx.sh"
 
--- .travis/script-osx.sh
+++ .travis/script-osx.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "** starting script-osx.sh"
 cmake --build . --config Release
 cmake --install . --config Release
 
