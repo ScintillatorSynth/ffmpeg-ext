a785939528d7a43baf79837ccf271403e0c7b917
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-26 13:48:06 -0700

    source a newer version of meson

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -4,6 +4,8 @@ sudo apt-get update
 sudo apt-get install --yes nasm
 
 if [ $CROSS_WINDOWS = true ]; then
+    sudo add-apt-repository ppa:nbritton/meson-0.47
+    sudo apt-get update
     sudo apt-get install subversion ragel curl texinfo g++ bison flex cvs yasm automake libtool autoconf gcc cmake git \
         make pkg-config zlib1g-dev mercurial unzip pax nasm gperf autogen bzip2 autoconf-archive p7zip-full meson      \
         clang python3-distutils -y
