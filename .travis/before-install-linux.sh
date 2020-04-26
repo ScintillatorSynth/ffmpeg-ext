4df7d6c9abc427346071e99766091a202c6d651a
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-26 14:15:45 -0700

    try pip3 for meson

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -4,11 +4,9 @@ sudo apt-get update
 sudo apt-get install --yes nasm
 
 if [ $CROSS_WINDOWS = true ]; then
-    sudo add-apt-repository --yes ppa:nbritton/meson-0.47
-    sudo apt-get update
-    sudo apt-get install --yes meson=0.47
     sudo apt-get install --yes subversion ragel curl texinfo g++ bison flex cvs yasm automake libtool autoconf gcc     \
         cmake git make pkg-config zlib1g-dev mercurial unzip pax nasm gperf autogen bzip2 autoconf-archive p7zip-full  \
-        clang python3-distutils
+        clang python3-distutils python3-pip
+    sudo pip3 install meson
 fi
 
