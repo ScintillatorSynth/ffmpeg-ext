baeba84f0a8fc993d6bee6d1026503d587a57977
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-26 13:59:40 -0700

    auto accept meson ppa add

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install --yes nasm
 
 if [ $CROSS_WINDOWS = true ]; then
-    sudo add-apt-repository ppa:nbritton/meson-0.47
+    sudo add-apt-repository --yes ppa:nbritton/meson-0.47
     sudo apt-get update
     sudo apt-get install subversion ragel curl texinfo g++ bison flex cvs yasm automake libtool autoconf gcc cmake git \
         make pkg-config zlib1g-dev mercurial unzip pax nasm gperf autogen bzip2 autoconf-archive p7zip-full meson      \
