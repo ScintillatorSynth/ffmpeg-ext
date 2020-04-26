2c057cad08a67c18e6c2a586e20873022640fffa
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-26 14:25:57 -0700

    add python setuptools

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -6,7 +6,7 @@ sudo apt-get install --yes nasm
 if [ $CROSS_WINDOWS = true ]; then
     sudo apt-get install --yes subversion ragel curl texinfo g++ bison flex cvs yasm automake libtool autoconf gcc     \
         cmake git make pkg-config zlib1g-dev mercurial unzip pax nasm gperf autogen bzip2 autoconf-archive p7zip-full  \
-        clang python3-distutils python3-pip
+        clang python3-distutils python3-pip python3-setuptools
     pip3 install --user meson
 fi
 
