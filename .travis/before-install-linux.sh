334f3e553c023944ddd1ffb88706cde9e63304d5
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-26 14:05:35 -0700

    try asking for a specific version

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -6,8 +6,9 @@ sudo apt-get install --yes nasm
 if [ $CROSS_WINDOWS = true ]; then
     sudo add-apt-repository --yes ppa:nbritton/meson-0.47
     sudo apt-get update
-    sudo apt-get install subversion ragel curl texinfo g++ bison flex cvs yasm automake libtool autoconf gcc cmake git \
-        make pkg-config zlib1g-dev mercurial unzip pax nasm gperf autogen bzip2 autoconf-archive p7zip-full meson      \
-        clang python3-distutils -y
+    sudo apt-get install --yes meson=0.47
+    sudo apt-get install --yes subversion ragel curl texinfo g++ bison flex cvs yasm automake libtool autoconf gcc     \
+        cmake git make pkg-config zlib1g-dev mercurial unzip pax nasm gperf autogen bzip2 autoconf-archive p7zip-full  \
+        clang python3-distutils
 fi
 
