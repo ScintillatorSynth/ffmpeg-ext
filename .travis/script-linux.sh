9aeebf5d42b6f40c8cf35c9d191c34827c200783
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-03 14:42:10 -0700

    fail on meson fail

--- .travis/script-linux.sh
+++ .travis/script-linux.sh
@@ -5,8 +5,7 @@ if [ $CROSS_WINDOWS = true ]; then
     cd $TRAVIS_BUILD_DIR/build/ffmpeg-windows-build-helpers
     PATH=$HOME/.local/bin:$PATH ./cross_compile_ffmpeg.sh --build-ffmpeg-shared=y --compiler-flavors=win64             \
         --enable-gpl=y --sandbox-ok=y --ffmpeg-git-checkout-version=${FFMPEG_GIT_TAG}                                  \
-        --ffmpeg-git-checkout=${FFMPEG_GIT_REPO} --gcc-cpu-count=2 --debug |& awk 'NR % 500 == 0' ||                   \
-        tail -n1000 sandbox/cross_compilers/build.log; exit 1
+        --ffmpeg-git-checkout=${FFMPEG_GIT_REPO} --gcc-cpu-count=2 --debug  || exit 1                                  \
     7z e sandbox/redist/ffmpeg-*-win64-shared.7z -o$TRAVIS_BUILD_DIR/build/install-ext
 else
     make
