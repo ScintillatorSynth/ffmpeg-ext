760c0778698b6f5a153480de64aab4adb1c0d69b
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-03 15:40:26 -0700

    try to fix meson build error

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -6,7 +6,7 @@ sudo apt-get install --yes nasm
 if [ $CROSS_WINDOWS = true ]; then
     sudo apt-get install --yes subversion ragel curl texinfo g++ bison flex cvs yasm automake libtool autoconf gcc     \
         cmake git make pkg-config zlib1g-dev mercurial unzip pax gperf autogen bzip2 autoconf-archive p7zip-full clang \
-        python3-distutils python3-pip python3-setuptools
+        python3-distutils python3-pip python3-setuptools python3-wheel
     pip3 install --user meson || exit 2
 fi
 
