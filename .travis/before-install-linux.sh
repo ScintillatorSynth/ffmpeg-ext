d17925bd85dae74b0e95e79cf92575f762dbca58
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-03 14:06:41 -0700

    try multi-lib compile

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -5,8 +5,8 @@ sudo apt-get install --yes nasm
 
 if [ $CROSS_WINDOWS = true ]; then
     sudo apt-get install --yes subversion ragel curl texinfo g++ bison flex cvs yasm automake libtool autoconf gcc     \
-        cmake git make pkg-config zlib1g-dev mercurial unzip pax nasm gperf autogen bzip2 autoconf-archive p7zip-full  \
-        clang python3-distutils python3-pip python3-setuptools
+        cmake git make pkg-config zlib1g-dev mercurial unzip pax gperf autogen bzip2 autoconf-archive p7zip-full clang \
+        python3-distutils python3-pip python3-setuptools
     pip3 install --user meson
 fi
 
--- .travis/script-linux.sh
+++ .travis/script-linux.sh
@@ -3,9 +3,9 @@
 if [ $CROSS_WINDOWS = true ]; then
     mkdir -p $TRAVIS_BUILD_DIR/build/install-ext
     cd $TRAVIS_BUILD_DIR/build/ffmpeg-windows-build-helpers
-    PATH=$HOME/.local/bin:$PATH ./cross_compile_ffmpeg.sh --build-ffmpeg-shared=y --compiler-flavors=win64             \
+    PATH=$HOME/.local/bin:$PATH ./cross_compile_ffmpeg.sh --build-ffmpeg-shared=y --compiler-flavors=multi             \
         --enable-gpl=y --sandbox-ok=y --ffmpeg-git-checkout-version=${FFMPEG_GIT_TAG}                                  \
-        --ffmpeg-git-checkout=${FFMPEG_GIT_REPO} --gcc-cpu-count=2
+        --ffmpeg-git-checkout=${FFMPEG_GIT_REPO} --gcc-cpu-count=2 || exit 1
     7z e sandbox/redist/ffmpeg-*-win64-shared.7z -o$TRAVIS_BUILD_DIR/build/install-ext
 else
     make
