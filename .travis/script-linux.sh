196c3daeb9f570056ef96f60d111e4190d70e947
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-05 23:22:23 -0700

    remove symbolic links from install-ext (#16)

--- .travis/script-linux.sh
+++ .travis/script-linux.sh
@@ -4,6 +4,8 @@ if [ $CROSS_WINDOWS = true ]; then
     cd $TRAVIS_BUILD_DIR/build/ffmpeg
     make
     make install
+    # The links are breaking when being extracted on windows, so remove them.
+    find $TRAVIS_BUILD_DIR/build/install-ext -type l -exec rm {} \;
 else
     make
 fi
