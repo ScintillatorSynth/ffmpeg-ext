6613c014ec52dddbeee60cde827ffb42306276b7
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-05 20:08:45 -0700

    move libpng build out of final build

--- .travis/before-script-linux.sh
+++ .travis/before-script-linux.sh
@@ -2,6 +2,10 @@
 
 if [ $CROSS_WINDOWS = true ]; then
     mkdir $TRAVIS_BUILD_DIR/build/install-ext
+    # libpng and others not necessary in final build as they are statically
+    # linked. Furthermore there seems to be some issue in compressing them
+    # so we put them in an intermediate build directory.
+    mkdir $TRAVIS_BUILD_DIR/build/pre
 
     # build zlib for libpng
     cd $TRAVIS_BUILD_DIR/build
@@ -11,8 +15,8 @@ if [ $CROSS_WINDOWS = true ]; then
     # spot of wisdom from https://wiki.openttd.org/Cross-compiling_for_Windows#libpng. The main makefile seems broken
     # for the cross-compilation use case. Modify the win32 makefile to use the mingw setup.
     sed -e s/"PREFIX ="/"PREFIX = x86_64-w64-mingw32-"/ -i win32/Makefile.gcc
-    BINARY_PATH=${TRAVIS_BUILD_DIR}/build/install-ext/bin INCLUDE_PATH=${TRAVIS_BUILD_DIR}/build/install-ext/include   \
-        LIBRARY_PATH=${TRAVIS_BUILD_DIR}/build/install-ext/lib make -f win32/Makefile.gcc install
+    BINARY_PATH=${TRAVIS_BUILD_DIR}/build/pre/bin INCLUDE_PATH=${TRAVIS_BUILD_DIR}/build/pre/include                   \
+        LIBRARY_PATH=${TRAVIS_BUILD_DIR}/build/pre/lib make -f win32/Makefile.gcc install
 
     # build libpng for png encodes
     cd $TRAVIS_BUILD_DIR/build
@@ -21,8 +25,8 @@ if [ $CROSS_WINDOWS = true ]; then
     git checkout libpng16
     ./configure --prefix=$TRAVIS_BUILD_DIR/build/install-ext --host=x86_64-w64-mingw32 --enable-shared=no              \
         CC=x86_64-w64-mingw32-gcc                                                                                      \
-        CPPFLAGS="-I${TRAVIS_BUILD_DIR}/build/install-ext/include"                                                     \
-        LDFLAGS="-L${TRAVIS_BUILD_DIR}/build/install-ext/lib"
+        CPPFLAGS="-I${TRAVIS_BUILD_DIR}/build/pre/include"                                                             \
+        LDFLAGS="-L${TRAVIS_BUILD_DIR}/build/pre/lib"
     make || exit 1
     make install || exit 2
 
@@ -33,8 +37,8 @@ if [ $CROSS_WINDOWS = true ]; then
     git checkout $FFMPEG_GIT_TAG
     ./configure --enable-gpl --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32-                      \
         --pkg-config=pkg-config --prefix=$TRAVIS_BUILD_DIR/build/install-ext --disable-programs --enable-shared        \
-        --extra-cflags="-I${TRAVIS_BUILD_DIR}/build/install-ext/include"                                               \
-        --extra-ldflags="-L${TRAVIS_BUILD_DIR}/build/install-ext/lib"
+        --extra-cflags="-I${TRAVIS_BUILD_DIR}/build/pre/include"                                                       \
+        --extra-ldflags="-L${TRAVIS_BUILD_DIR}/build/pre/lib"
 else
     cmake -DSCIN_FFMPEG_GIT_REPO=$FFMPEG_GIT_REPO -DSCIN_FFMPEG_GET_TAG=$FFMPEG_GIT_TAG ..
 fi
